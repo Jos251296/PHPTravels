e6efc2ac4de8873332509bf486eeff8a39ede87b
Jos251296 <32730895+Jos251296@users.noreply.github.com>
2020-11-05 09:08:59 +0100

    Create VisaApplicationTest.java

---
+++ src/test/java/seleniumexercises/exercises/VisaApplicationTest.java
@@ -0,0 +1,49 @@
+package seleniumexercises.exercises;
+import seleniumexercises.pages.*;
+import org.junit.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class VisaApplicationTest{
+
+    private WebDriver driver;
+
+    @Before
+    public void startBrowser() {
+
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chromedriver.exe");
+
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void applyForVisa() {
+
+        new HomePage(driver)
+                .load()
+                .selectMenuItem("Visa");
+
+        new SearchForVisaPage(driver)
+                .setCountryOfOriginTo("American Samoa")
+                .setCountryToVisitTo("Saudi Arabia")
+                .setDate("02-11-2020")
+                .submit();
+
+        new VisaApplicationPage(driver)
+                .setFirstName("Holly")
+                .setLastName("Day")
+                .setEmail("holly_day@badpuns.com")
+                .setConfirmEmail("holly_day@badpuns.com")
+                .setContactNumber("465-555-8378")
+                .additionalRequestsButton()
+                .setAdditionalRequest("A pot of tea")
+                .bookingButton();
+    }
+
+    @After
+        public void stopBrowser() {
+
+        driver.quit();
+    }
+}
