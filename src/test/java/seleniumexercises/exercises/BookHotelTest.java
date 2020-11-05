39d44daff3909fa6f55afe9cb56c3ceeba9711e7
lutevankempen <71704301+lutevankempen@users.noreply.github.com>
2020-11-05 13:17:30 +0100

    Update BookHotelTest.java

--- src/test/java/seleniumexercises/exercises/BookHotelTest.java
+++ src/test/java/seleniumexercises/exercises/BookHotelTest.java
@@ -34,29 +34,20 @@ public class BookHotelTest {
 
         new BookHotelPage(driver)
                 .selectRadioButton("2")
-<<<<<<< HEAD
 //                .clickViewMoreButton()
 //                .selectFilterButton("Night Club")
 //                .selectFilterButton("SPA")
-                .selectPriceRange(50, 1000);
+                .selectPriceRange(50,1000);
 //                .clickPropertyType("Hotel")
 //                .clickPriceFilter("Low to High")
 //                .clickSearchButton("Yes")
-//                .clickDetailsButton();
-=======
-                .clickViewMoreButton()
-                .selectFilterButton("Night Club")
-                .selectFilterButton("SPA")
-                .clickPropertyType("Hotel")
-                .clickPriceFilter("Low to High")
-                .clickSearchButton("Yes")
-                .clickDetailsButton()
-                .setDropdownCheckIn()
-                .setDropdownCheckOut()
-                .setAdultNum(2)
-                .setChildrenNum(0)
-                .clickModifyButton();
->>>>>>> 2964c2dfe58e17d4d7374be05344e00499ee5a4c
+//                .clickDetailsButton()
+//                .setDropdownCheckIn()
+//                .setDropdownCheckOut()
+//                .setAdultNum(2)
+//                .setChildrenNum(0)
+//                .clickModifyButton();
+
     }
 
 //    @After
