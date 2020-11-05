0458c0bbaf11df99e98100ff3d7aab218479c077
lutevankempen <71704301+lutevankempen@users.noreply.github.com>
2020-11-05 16:41:38 +0100

    Update SearchForVisaPage.java

--- src/test/java/seleniumexercises/pages/SearchForVisaPage.java
+++ src/test/java/seleniumexercises/pages/SearchForVisaPage.java
@@ -4,17 +4,20 @@ import seleniumexercises.helpers.SeleniumHelpers;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
+
 public class SearchForVisaPage {
 
     private WebDriver driver;
     private SeleniumHelpers selenium;
 
     private By dropdownFromCountry = By.xpath("//select[@name='nationality_country']/following-sibling::div");
+    private By probeersel   = By.xpath("//select[@name='nationality_country']");
     private By textfieldFromCountry = By.xpath("//select[@name='nationality_country']/following-sibling::div//input[@class='chosen-search-input']");
     private By dropdownToCountry = By.xpath("//select[@name='destination_country']/following-sibling::div");
     private By textfieldToCountry = By.xpath("//select[@name='destination_country']/following-sibling::div//input[@class='chosen-search-input']");
     private By textfieldDate = By.name("date");
     private By buttonSubmit = By.xpath("//button[text()='Submit']");
+
 
     public SearchForVisaPage(WebDriver driver) {
         this.driver = driver;
@@ -26,7 +29,10 @@ public class SearchForVisaPage {
         selenium.click(dropdownFromCountry);
         selenium.sendKeys(textfieldFromCountry, countryOfOrigin);
         selenium.click(By.xpath(String.format("//li/em[text()='%s']", countryOfOrigin)));
+
+        selenium.dropdown(probeersel, countryOfOrigin);
         return this;
+
     }
 
     public SearchForVisaPage setCountryToVisitTo(String countryToVisit) {
@@ -34,6 +40,8 @@ public class SearchForVisaPage {
         selenium.click(dropdownToCountry);
         selenium.sendKeys(textfieldToCountry, countryToVisit);
         selenium.click(By.xpath(String.format("//li/em[text()='%s']", countryToVisit)));
+
+//        selenium.dropdown(dropdownToCountry, countryToVisit);
         return this;
     }
 
