dade1e80d4dd4c841b99af9434bcd9d8dbc6a1ea
Jos251296 <32730895+Jos251296@users.noreply.github.com>
2020-11-05 11:11:41 +0100

    fix selectors

--- src/test/java/seleniumexercises/pages/BookHotelPage.java
+++ src/test/java/seleniumexercises/pages/BookHotelPage.java
@@ -9,11 +9,14 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import seleniumexercises.helpers.SeleniumHelpers;
 
+import java.awt.print.Book;
+
 public class BookHotelPage {
 
     private WebDriver driver;
     private SeleniumHelpers selenium;
-    private By filterButton = By.xpath("//input[@id='Airport Transport']");
+    private By viewMoreButton = By.xpath("//span[contains(text(),'View More (+)')]");
+    private By detailsButton = By.xpath("/html/body/div[1]/div[1]/div[1]/section/div/div[2]/div[2]/div/div[2]/div[1]/div/div[2]/div/div[3]/div/div[2]/a");
 
     public BookHotelPage(WebDriver driver) {
         this.driver = driver;
@@ -30,9 +33,27 @@ public class BookHotelPage {
     }*/
 
     public BookHotelPage selectFilterButton(String filterType){
-        selenium.click(By.xpath("//input[@id='" + filterType + "']"));
+        selenium.click(By.xpath("//label[@for='" + filterType + "']"));
         return this;
     }
 
+    public BookHotelPage clickViewMoreButton(){
+        selenium.click(viewMoreButton);
+        return this;
+    }
 
+    public BookHotelPage clickPropertyType(String propertyType){
+        selenium.click(By.xpath("//label[@for='" + propertyType + "']"));
+        return this;
+    }
+
+    public BookHotelPage clickPriceFilter(String priceFilter){
+        selenium.click(By.xpath("//label[contains(text(),'" + priceFilter + "')]"));
+        return this;
+    }
+
+    public BookHotelPage clickDetailsButton(){
+        selenium.click(detailsButton);
+        return this;
+    }
 }
