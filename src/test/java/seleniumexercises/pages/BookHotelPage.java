a75838337d1ed64cf22c2e4c76410c594610fdb3
Jos251296 <32730895+Jos251296@users.noreply.github.com>
2020-11-05 09:08:56 +0100

    Create BookHotelPage.java

---
+++ src/test/java/seleniumexercises/pages/BookHotelPage.java
@@ -0,0 +1,39 @@
+package seleniumexercises.pages;
+
+import net.bytebuddy.asm.Advice;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import seleniumexercises.helpers.SeleniumHelpers;
+
+public class BookHotelPage {
+
+    private WebDriver driver;
+    private SeleniumHelpers selenium;
+    private By starRadioButton = By.xpath("//input[@id='1']");
+    private By filterButton = By.xpath("//input[@id='Airport Transport']");
+
+    public BookHotelPage(WebDriver driver) {
+        this.driver = driver;
+        selenium = new SeleniumHelpers(driver);
+    }
+
+    public BookHotelPage selectRadioButton(String starNum){
+        selenium.click(By.xpath("//input[@id='" + starNum + "']"));
+        return this;
+    }
+
+    /*public BookHotelPage selectPriceRange(){
+
+    }*/
+
+    public BookHotelPage selectFilterButton(String filterType){
+        selenium.click(By.xpath("//input[@id='" + filterType + "']"));
+        return this;
+    }
+
+
+}
