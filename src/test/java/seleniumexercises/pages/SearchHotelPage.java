1d8531dde73c92b4904e76dfcc63db7f30c5448c
lutevankempen <71704301+lutevankempen@users.noreply.github.com>
2020-11-09 16:24:11 +0100

    Update SearchHotelPage.java

--- src/test/java/seleniumexercises/pages/SearchHotelPage.java
+++ src/test/java/seleniumexercises/pages/SearchHotelPage.java
@@ -5,20 +5,18 @@ import org.openqa.selenium.WebDriver;
 import seleniumexercises.helpers.SeleniumHelpers;
 
 public class SearchHotelPage {
+
     private WebDriver driver;
     private SeleniumHelpers selenium;
 
     private By textfieldDestination = By.xpath("//div[@id='hotels']/div//a");
-    private By textfieldHotelOrLocation = By.xpath("//*[@id='select2-drop']/div/input");
     private By dropdownCheckIn = By.id("checkin");
-    private By selectDataCheckin = By.xpath("//*[@id=\"datepickers-container\"]/div[1]/div/div/div/div[contains(text(),'20')]");
     private By dropdownCheckOut = By.id("checkout");
-    private By selectDataCheckOut = By.xpath("//*[@id=\"datepickers-container\"]/div[2]/div/div/div/div[contains(text(),'24')]");
 
-    private By buttonAdults = By.xpath("//input[@name= 'adults']/parent::div/span//button[contains(@class, 'bootstrap-touchspin-up')]");
-    private By buttonChildren = By.xpath("//input[@name= 'children']/parent::div/span//button[contains(@class, 'bootstrap-touchspin-up')]");
+    private By buttonAdultsUp = By.xpath("//input[@name= 'adults']/parent::div/span//button[contains(@class, 'bootstrap-touchspin-up')]");
+    private By buttonChildrenUp = By.xpath("//input[@name= 'children']/parent::div/span//button[contains(@class, 'bootstrap-touchspin-up')]");
+    private By buttonAdultsDown = By.xpath("//input[@name= 'adults']/parent::div/span//button[contains(@class, 'bootstrap-touchspin-down')]");
     private By submitHotelButton = By.xpath("//form[@name= 'HOTELS']/div/div/div[@class='col-lg-2 col-sm-12 col-xs-12']/button[contains(text(), 'Search')]");
-    private By buttonLessAdults = By.xpath("//input[@name= 'adults']/parent::div/span//button[contains(@class, 'bootstrap-touchspin-down')]");
 
 
     public SearchHotelPage(WebDriver driver) {
@@ -29,19 +27,56 @@ public class SearchHotelPage {
 
     public SearchHotelPage setTextfieldDestination (String destination){
 
-        selenium.click(textfieldDestination);
-        selenium.sendKeys(textfieldDestination, destination);
+        selenium.dropdown(textfieldDestination, textfieldDestination, destination);
         selenium.click(By.xpath("//div/span[@class='select2-match']"));
         return this;
-
     }
 
-    public SearchHotelPage setDropdownCheckIn (){
+    public SearchHotelPage setDropdownDates (int dateCheckIn, int dateCheckOut){
 
         selenium.click(dropdownCheckIn);
-        selenium.click(selectDataCheckin);
+        selenium.click(By.xpath(String.format("//div[@id='datepickers-container']//div[contains(text(),'%d')]", dateCheckIn)));
+
+        selenium.click(dropdownCheckOut);
+        selenium.click(By.xpath(String.format("//div[@id='datepickers-container']/div[2]//div[contains(text(),'%d')]", dateCheckOut)));
         return this;
     }
+
+    public SearchHotelPage setAmountOfVisitors(int adultNum, int childrenNum){
+
+        int placeholderValue =
+                Integer.parseInt(driver.findElement(By.xpath("//input[@name='adults']")).getAttribute("value"));
+
+        if(placeholderValue > 0){
+            for(int i=0; i<placeholderValue; i++){
+                selenium.click(buttonAdultsDown);
+            }
+        }
+
+        for(int i = 0; i<adultNum;i++){
+            selenium.click(buttonAdultsUp);
+        }
+
+        for(int i=0; i<childrenNum;i++){
+            selenium.click(buttonChildrenUp);
+        }
+        return this;
+    }
+
+    public SearchHotelPage clickSubmitHotel(){
+        selenium.click(submitHotelButton);
+        return this;
+    }
+
+    /************************************************
+     * DE CODE HIERONDER IS VERWIJDERD GEDURENDE HET
+     * LEESBAAR MAKEN VAN DE PAGE EN TEST OBJECTEN.
+     * ALS COMMENTAAR BEWAARD VOOR EVT. BEOORDELING
+     ************************************************/
+
+    /*
+        private By selectDataCheckOut = By.xpath("//*[@id=\"datepickers-container\"]/div[2]/div/div/div/div[contains(text(),'24')]");
+            private By selectDataCheckin = By.xpath("//div[@id=\"datepickers-container\"]//div[contains(text(),'20')]");
 
     public SearchHotelPage setDropdownCheckOut() {
         selenium.click(dropdownCheckOut);
@@ -49,7 +84,8 @@ public class SearchHotelPage {
         return this;
     }
 
-    public SearchHotelPage setAdultNum(int adultNum){
+        public SearchHotelPage setAdultNum(int adultNum){
+
         if(adultNum >= 2) {
             for (int i = 0; i < (adultNum - 2); i++)
                 selenium.click(buttonAdults);
@@ -68,8 +104,12 @@ public class SearchHotelPage {
         return this;
     }
 
-    public SearchHotelPage clickSubmitHotel(){
-        selenium.click(submitHotelButton);
+        public SearchHotelPage setTextfieldDestination (String destination){
+
+        selenium.click(textfieldDestination);
+        selenium.sendKeys(textfieldDestination, destination);
+        selenium.click(By.xpath("//div/span[@class='select2-match']"));
         return this;
     }
+     */
 }
